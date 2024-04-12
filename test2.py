8cad1076a8215b3d0dc9b306133a6ea0b8514abd
Alex Matv <matveev.98.o@bk.ru>
2024-04-12 19:48:53 +0300

    Кажется Хэнк научился перечислять информацию после двоеточия. Ещё он абсолютно точно считывает данные раздела "модули" из json файла. Я горжусь им.

--- test2.py
+++ test2.py
@@ -1,17 +1,19 @@
-import pandas as pd
-import numpy as np
-import json
-with open('course.json', encoding='utf-8') as f:
-    data = json.load(f)
-
-data = pd.DataFrame(data)
-def promt_data(column):
-    promt = f""
-    for course_name in data["Course_name"].values:
-        column_info = data[data['Course_name'] == course_name][column].values[0]
-        if type(column_info) == list:
-            column_info = ", ".join(column_info)
-        promt += f"Для курса с названием '{course_name}' такие данные о '{column}': {column_info}\n"
-    return promt
-
-print(promt_data('Description'))
+# import pandas as pd
+# import numpy as np
+# import json
+# with open('course.json', encoding='utf-8') as f:
+#     data = json.load(f)
+#
+# data = pd.DataFrame(data)
+# def promt_data(column):
+#     promt = f""
+#     for course_name in data["Course_name"].values:
+#         column_info = data[data['Course_name'] == course_name][column].values[0]
+#         if type(column_info) == list:
+#             column_info = ", ".join(column_info)
+#         promt += f"Для курса с названием '{course_name}' такие данные о '{column}': {column_info}\n"
+#     return promt
+#
+# print(promt_data('Description'))
+s = 'Здравствуйте! Меня зовут Хэнк, я Менеджер - консультант компании Газпромбанк. Рад приветствовать вас в нашем чате! Хотите узнать о наших учебных курсах?'
+print("jfasjlkfj '\\n' fjakljdf")
