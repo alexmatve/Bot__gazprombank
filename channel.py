c8e8c3a82012393ab081c63613a0b3eada8c9089
Alex Matv <matveev.98.o@bk.ru>
2024-04-15 02:52:27 +0300

    Конечное состояние программы.

--- channel.py
+++ channel.py
@@ -5,7 +5,7 @@ from aiogram import Bot, Dispatcher, F
 from aiogram.filters import Command
 from aiogram.fsm.storage.memory import MemoryStorage
 
-from HR import SalesGPT, llm
+from Consultant import SalesGPT, llm
 
 bot_token = '6680547488:AAGw_y0ncoSJf7UqFUvb36Yggg9PQI0BjNY'
 
@@ -18,40 +18,38 @@ async def main():
     bot = Bot(bot_token, parse_mode=None)
     logging.basicConfig(level=logging.INFO)
 
-    # @dp.message(Command(commands=["start"]))
     @dp.channel_post(Command(commands=["start"]))  # для группы
     async def repl(message):
         global sales_agent
         sales_agent = SalesGPT.from_llm(llm, verbose=False)
         sales_agent.seed_agent()
-        #await message.answer('Генерируется ответ♻️')  # Даём понять пользователю, что бот работает
+        #await message.answer('Генерируется ответ♻️')  # Даём понять пользователю, что бот работает (удалили, чтобы тестирующий бот не отвечал на данное сообщение)
         ai_message = sales_agent.ai_step()
-        print(ai_message)
-        #await message.answer(ai_message)
+        print(f"BOT: {ai_message}")
+        #await message.answer(ai_message) # удаляем данную строчку, чтобы не создавалось конфликта между ботами в чате (чтобы не начинали переписываться по два сообщения). Если тестирующий бот не начинает сообщение первым после команды /start, то данную строчку нужно добавить
         #await bot.delete_message(chat_id=message.chat.id, message_id=message.message_id + 1)
 
-    # @dp.message(F.text)
     @dp.channel_post(F.text)  # для группы
     async def repl(message):
         if sales_agent is None:
             await message.answer('Используйте команду /start')
         else:
             human_message = message.text
+            print(f"User: {message.text}") # Выводим в консоль сообщение пользователя (необязательная опция)
             if human_message:
                 sales_agent.human_step(human_message)
                 sales_agent.analyse_stage()
-            #await message.answer('Генерируется ответ♻️')
+            #await message.answer('Генерируется ответ♻️') # Даём понять пользователю, что бот работает (удалили, чтобы тестирующий бот не отвечал на данное сообщение)
             ai_message = sales_agent.ai_step()
-            print(ai_message)
+            print(f"BOT: {ai_message}") # Выводим в консоль сообщение бота (необязательная опция)
             await message.answer(ai_message)
             #await bot.delete_message(chat_id=message.chat.id, message_id=message.message_id + 1)
 
-    # @dp.message(~F.text)
+
     @dp.channel_post(~F.text)  # для группы
     async def empty(message):
         await message.answer('Бот принимает только текст')
 
-    # await dp.start_polling(bot)
     await bot.delete_webhook(drop_pending_updates=True)  # для группы
     await dp.start_polling(bot, allowed_updates=['channel_post'])  # для группы
 
--- chat.py
+++ chat.py
@@ -6,7 +6,7 @@ from aiogram import Bot, Dispatcher, F
 from aiogram.filters import Command
 from aiogram.fsm.storage.memory import MemoryStorage
 
-from test_HR import SalesGPT, llm
+from Consultant import SalesGPT, llm
 
 bot_token = '6680547488:AAGw_y0ncoSJf7UqFUvb36Yggg9PQI0BjNY'
 
@@ -20,41 +20,37 @@ async def main():
     logging.basicConfig(level=logging.INFO)
 
     @dp.message(Command(commands=["start"]))
-    #@dp.channel_post(Command(commands=["start"]))  # для группы
     async def repl(message):
         global sales_agent
         sales_agent = SalesGPT.from_llm(llm, verbose=False)
         sales_agent.seed_agent()
         await message.answer('Генерируется ответ♻️')  # Даём понять пользователю, что бот работает
         ai_message = sales_agent.ai_step()
-        print(ai_message)
+        print(f"BOT: {ai_message}")
         await message.answer(ai_message)
         await bot.delete_message(chat_id=message.chat.id, message_id=message.message_id + 1)
 
     @dp.message(F.text)
-    #@dp.channel_post(F.text)  # для группы
     async def repl(message):
         if sales_agent is None:
             await message.answer('Используйте команду /start')
         else:
             human_message = message.text
+            print(f"User: {message.text}") # Выводим в консоль сообщение пользователя (необязательная опция)
             if human_message:
                 sales_agent.human_step(human_message)
                 sales_agent.analyse_stage()
             await message.answer('Генерируется ответ♻️')
             ai_message = sales_agent.ai_step()
-            print(ai_message)
+            print(f"BOT: {ai_message}") # Выводим в консоль сообщение бота (необязательная опция)
             await message.answer(ai_message)
             await bot.delete_message(chat_id=message.chat.id, message_id=message.message_id + 1)
 
     @dp.message(~F.text)
-    #@dp.channel_post(~F.text)  # для группы
     async def empty(message):
         await message.answer('Бот принимает только текст')
 
     await dp.start_polling(bot)
-    #await bot.delete_webhook(drop_pending_updates=True)  # для группы
-    #await dp.start_polling(bot, allowed_updates=['channel_post'])  # для группы
 
 if __name__ == "__main__":
     asyncio.run(main())
